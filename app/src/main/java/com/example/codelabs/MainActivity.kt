7576aa5e78ef7a7583a38dfaa333062ca6b9989d
Vincent <vincentcoby@outlook.de>
2022-12-05 17:24:19 +0100

    Added Viewbinding for Main Activity by adding viewBinding true in buildfeature in android and going to add navhostfragment to main xml next wish me luck

--- app/src/main/java/com/example/codelabs/MainActivity.kt
+++ app/src/main/java/com/example/codelabs/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.codelabs
 
-import android.content.Context
 import android.os.Bundle
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import com.example.codelabs.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity(){
+    lateinit var binding :ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
